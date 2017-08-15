383809da9ef21bb3b0d5a9f5fba88b1283a10ef2
automationDeveloper <test@automation.com>
2017-08-15 13:48:00 +0530

    commit changes

--- src/main/java/com/framework/log4jdemo/TestClass.java
+++ src/main/java/com/framework/log4jdemo/TestClass.java
@@ -15,8 +15,8 @@ public class TestClass
 		//PropertiesConfigurator is used to configure logger from properties file
         PropertyConfigurator.configure("src/main/java/resources/log4j.properties");
         
-		LOG.info("Info message");
-		LOG.warn("Warn messgae");
-		LOG.error("Error message");
+		LOG.info("Info message.");
+		LOG.warn("Warn messgae.");
+		LOG.error("Error message.");
 	}
 }
